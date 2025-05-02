420ca9712ba2971f62ecfde831ebb2c7edf58d18
unknown <saulosorioh@hotmail.com>
2025-05-02 03:02:36 -0500

    a

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 from flask import Flask
 from views.upload import upload_bp
 from views.generar_pedidos import generar_pedidos_bp
+from views.consolidar_compras import consolidar_bp
 
 app = Flask(__name__, static_folder="static")
 app.secret_key = os.getenv("SECRET_KEY", "cámbiala")
 app.register_blueprint(upload_bp)
 app.register_blueprint(generar_pedidos_bp)
+app.register_blueprint(consolidar_bp)
 
 if __name__ == "__main__":
     app.run(
