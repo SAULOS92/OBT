96a7c1a7334451dcbbcf8af63b69d600d179970e
unknown <saulosorioh@hotmail.com>
2025-05-02 00:24:42 -0500

    vamos

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 from flask import Flask
 from views.upload import upload_bp
-from views.export import export_bp
 from views.generar_pedidos import generar_pedidos_bp
 
 app = Flask(__name__, static_folder="static")
 app.secret_key = os.getenv("SECRET_KEY", "cámbiala")
 app.register_blueprint(upload_bp)
-app.register_blueprint(export_bp)
 app.register_blueprint(generar_pedidos_bp)
 
 if __name__ == "__main__":
